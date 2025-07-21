5affbe3b8fa3fca8ac760870b1d41c05fd3ed71a
Akshay Darade <akshaydarade6675@gmail.com>
2025-07-21 15:41:44 +0000

    add buttons delete and update

--- contactapp/contactapp/urls.py
+++ contactapp/contactapp/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
 from django.urls import path
+from addContactApp.views import HomeView, ContactCreateView, ContactUpdateView, ContactDeleteView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('', HomeView.as_view(), name='home'),
+    path('add/', ContactCreateView.as_view(), name='add_contact'),
+    path('edit/<int:pk>/', ContactUpdateView.as_view(), name='edit_contact'),
+    path('delete/<int:pk>/', ContactDeleteView.as_view(), name='delete_contact'),
 ]
